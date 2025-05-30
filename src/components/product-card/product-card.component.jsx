46b504600e240a24058f09da40d72a321743aeea
Gianni Guatieri <gianniguatieri@Giannis-MacBook-Pro.local>
2025-05-30 16:48:43 +0200

    migrating cart to Redux, Redux-persist, Thunk

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,26 +1,30 @@
-import { useContext } from 'react';
-import { CartContext } from '../../contexts/cart.context';
-import Button ,{ BUTTON_TYPE_CLASSES } from '../button/button.component';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { selectCartItems } from '../../store/cart/cart.selector';
+import { addItemToCart } from '../../store/cart/cart.action';
+
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
 
 import './product-card.styles.scss'
 
 const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product;
-    const { addItemToCart } = useContext(CartContext);
+    const cartItems = useSelector(selectCartItems);
+    const dispatch = useDispatch();
 
-    const addProductToCart = () => addItemToCart(product);
+    const addProductToCart = () => dispatch(addItemToCart(cartItems, product));
 
-    return(<div className="product-card-container">
-    <img src={imageUrl} alt={`{name}`}/>
-    <div className='footer'>
-        <img src={imageUrl} alt={`${name}`} />
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
-    </div>
-    <Button buttonType={BUTTON_TYPE_CLASSES.inverted} onClick={addProductToCart}>
-        Add to card
-    </Button>
+    return (<div className="product-card-container">
+        <img src={imageUrl} alt={`{name}`} />
+        <div className='footer'>
+            <img src={imageUrl} alt={`${name}`} />
+            <span className='name'>{name}</span>
+            <span className='price'>{price}</span>
+        </div>
+        <Button buttonType={BUTTON_TYPE_CLASSES.inverted} onClick={addProductToCart}>
+            Add to card
+        </Button>
     </div>
     );
 }
