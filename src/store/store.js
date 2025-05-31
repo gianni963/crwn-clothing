aa8a2c153d138e3d752d2a4bdaf7ae9530475880
Gianni Guatieri <gianniguatieri@Giannis-MacBook-Pro.local>
2025-05-31 15:57:17 +0200

    add Redux-Saga, fetchCategoriesAsync Thunk to Saga

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,24 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
-import { thunk } from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+import { rootSaga } from './user/root-saga';
 
 import { rootReducer } from './root-reducer';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+
+const sagaMiddleware = createSagaMiddleware();
+
 const middleWares = [
   process.env.NODE_ENV === 'development' && logger,
-  thunk,
+  sagaMiddleware
 ].filter(Boolean);
 
 const composeEnhancer =
@@ -17,11 +28,7 @@ const composeEnhancer =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['user'],
-};
+
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
@@ -32,5 +39,7 @@ export const store = createStore(
   undefined,
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
